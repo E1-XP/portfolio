11e4b4b4af4b3a2e917c1a5019c4f496de4459af
E!-XP <poczta.elektroniczna@hotmail.com>
2019-10-25 13:22:05 +0200

    optimize images

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
     <transition name="h-fade-anim-delay" appear>
       <nuxt-link to="/" v-if="shouldShowHomeBtn" class="navigation-link">
         <span class="navigation-link__icon">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+          <svg width="24" height="24" viewBox="0 0 24 24">
             <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
             <path d="M0 0h24v24H0z" fill="none" />
           </svg>
