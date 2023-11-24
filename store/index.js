d793b22a77ca2c2fc57fd0245e7f6edf138d2461
E1-XP <sebastian-stypula@hotmail.com>
2023-11-24 13:03:14 +0100

    update technology slider, optimize main background

--- store/index.js
+++ store/index.js
@@ -45,23 +45,23 @@ export const state = () => ({
   stack: [
     { name: "JavaScript", img: "/img/logo-javascript.svg" },
     { name: "TypeScript", img: "/img/typescript.svg" },
+    { name: "Next.js", img: "/img/next.svg" },
     { name: "React.js", img: "/img/react.svg" },
     { name: "Redux.js", img: "/img/redux.svg" },
     { name: "RxJS", img: "/img/rxjs-1.svg" },
-    { name: "Vue.js", img: "/img/vue-9.svg" },
     { name: "Nuxt.js", img: "/img/nuxt.svg" },
+    { name: "Vue.js", img: "/img/vue-9.svg" },
     { name: "Node.js", img: "/img/nodejs-icon.svg" },
     { name: "Express.js", img: "/img/express.svg" },
-    { name: "Sequelize", img: "/img/sequelize.svg" },
+    { name: "Prisma", img: "/img/prisma.svg" },
     { name: "MySQL", img: "/img/mysql.svg" },
     { name: "Mongo DB", img: "/img/mongodb.svg" },
-    { name: "Redis", img: "/img/redis.svg" },
     { name: "Webpack.js", img: "/img/webpack-icon.svg" },
+    { name: "Tailwind CSS", img: "/img/tailwind.svg" },
     { name: "Sass", img: "/img/sass-1.svg" },
     { name: "HTML5", img: "/img/html5.svg" },
     { name: "Git", img: "/img/git-icon.svg" },
     { name: "Figma", img: "/img/figma-1.svg" },
-    { name: "Avocode", img: "/img/avocode-inc-1.svg" },
     { name: "Photoshop", img: "/img/photoshop-cc.svg" }
   ]
 });
